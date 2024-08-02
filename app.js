c9498dce754ed6642a90362cd7a60d73fd3aa6b6
valienteCDV <58440743+valienteCDV@users.noreply.github.com>
2024-08-01 21:34:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -208,7 +208,7 @@ function updateLastUpdateTime() {
     const now = new Date();
     document.getElementById('update-time').textContent = now.toLocaleTimeString('es-AR', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
 }
-}
+
 
 
 /**
